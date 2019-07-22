0f83dd3f8e39d2415f6911ede87114292b665d1c
Jonathon Green <ericg9811@gmail.com>
2019-07-22 16:26:26 -0500

    created MovieCard component and returned the component in the Movie.js and MovieList.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
+
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState();
- 
-console.log(props)    
+    const [movie, setMovie] = useState();
+    const { match } = props;
+    const { params } = match;
+     
   useEffect(() => {
-    const id = props.match.params.id;
+    const id = params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -34,22 +37,7 @@ console.log(props)
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
